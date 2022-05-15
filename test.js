961c6fbc763e3073a4ff115ecebbc0053f4e5328
hansol-FE <dundunki@naver.com>
2022-05-15 14:49:13 +0900

    [study] javascript 연습문제

---
+++ test.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 20;
+var c = 30;
+//export var d = 40;
+
+export  {a,b}; // import {변수명} from '경로' /  import {변수 as 새변수명} form '경로'
+export default c; // import 내맘대로작명 from '경로'
