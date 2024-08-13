f2a4587c7bc9fcaea226c765ecd3528beb004799
hansol <dundunki@naver.com>
2024-08-13 14:33:10 +0900

    router 생성

--- js/app.js
+++ js/app.js
@@ -13,35 +13,50 @@ function getData(url) {
   return JSON.parse(ajax.response); // JSON -> Object
 }
 
-const newsFeed = getData(NEWS_URL);
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-// createElement
-const ul = document.createElement("ul");
+  newsList.push("<ul>");
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+        <li>
+          <a href="#${newsFeed[i].id}">
+              ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          </a>
+        </li>
+        `);
+  }
+  newsList.push("</ul>");
 
-// 해시 변경 이벤트 리스너
-window.addEventListener("hashchange", function () {
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = this.location.hash.substring(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
-  const title = this.document.createElement("h1");
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-    </a>
-  </li>
-  `;
-
-  ul.appendChild(div.firstElementChild);
+  container.innerHTML = `
+      <h1>${newsContent.title}</h1>
+  
+      <div>
+          <a href="#">목록으로 </a>
+      </div>
+    `;
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+// 해시 변경 이벤트 리스너 (화면 전환 트리거)
+window.addEventListener("hashchange", router);
+
+router();
