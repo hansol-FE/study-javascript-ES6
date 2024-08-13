f5f1bd2817647b88a197461da933d400eb4e136b
hansol <dundunki@naver.com>
2024-08-13 15:27:33 +0900

    문자열 방식 ui -> 템플릿 방식 ui 구현 /  tailwind css 사용

--- js/app.js
+++ js/app.js
@@ -21,6 +21,20 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
 
+  let template = `
+    <div class="container mx-auto p-4">
+        <h1>News</h1>
+        <ul>
+            {{__news_feed__}}
+        </ul>
+        <div>
+            <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+            <a href="#/page/{{__next_page__}}">다음 페이지</a>
+        </div>
+    </div>
+
+  `;
+
   newsList.push("<ul>");
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
@@ -31,19 +45,15 @@ function newsFeed() {
         </li>
         `);
   }
-  newsList.push("</ul>");
 
-  // 페이징
-  newsList.push(`
-  <div>
-    <a href="#/page/${
-      store.currentPage > 1 ? store.currentPage - 1 : 1
-    }" >이전 페이지 </a>
-    <a href="#/page/${store.currentPage + 1}" >다음 페이지 </a>
-   </div>
-  `);
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-  container.innerHTML = newsList.join("");
+  container.innerHTML = template;
 }
 
 function newsDetail() {
