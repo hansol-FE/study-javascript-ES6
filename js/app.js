f6a08d5eba486feca186358d83e0feb9298a210f
hansol <dundunki@naver.com>
2024-08-13 13:26:25 +0900

    해시 변경 이벤트 리스너 부착

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const ajax = new XMLHttpRequest(); // XHR
 
+const container = document.getElementById("root");
+const content = document.createElement("div");
+
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
 ajax.open("GET", NEWS_URL, false); // 동기적 처리
 ajax.send();
 
@@ -10,11 +15,31 @@ const newsFeed = JSON.parse(ajax.response);
 // createElement
 const ul = document.createElement("ul");
 
+// 해시 변경 이벤트 리스너
+window.addEventListener("hashchange", function () {
+  const id = this.location.hash.substring(1);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  console.log(newsContent);
+  const title = this.document.createElement("h1");
+
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement("a");
 
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
