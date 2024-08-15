99eefc402c138aed84e80645eb17540a575f436b
hansol <dundunki@naver.com>
2024-08-15 13:31:49 +0900

    변수 타입 지정

--- ts/app.ts
+++ ts/app.ts
@@ -1,12 +1,28 @@
-const ajax = new XMLHttpRequest(); // XHR
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+};
 
-const container = document.getElementById("root");
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+};
+
+const ajax: XMLHttpRequest = new XMLHttpRequest(); // XHR
+
+const container: HTMLElement | null = document.getElementById("root");
 const content = document.createElement("div");
 
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
@@ -26,8 +42,16 @@ function makeFeeds(feeds) {
   return feeds;
 }
 
+function updateView(html) {
+  if (container) {
+    container.innerHTML = template;
+  } else {
+    console.error("empty root container");
+  }
+}
+
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
 
   let template = `
@@ -92,7 +116,7 @@ function newsFeed() {
   );
   template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-  container.innerHTML = template;
+  updateView(template);
 }
 
 function newsDetail() {
@@ -157,9 +181,8 @@ function newsDetail() {
     return commentString.join("");
   }
 
-  container.innerHTML = template.replace(
-    "{{__comments__}}",
-    makeComment(newsContent.comments)
+  updateView(
+    template.replace("{{__comments__}}", makeComment(newsContent.comments))
   );
 }
 
